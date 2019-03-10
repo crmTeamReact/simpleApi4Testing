d617efca61939f1f67b733f2c6dbf2fcc580bead
Alexis Corbacho <alexis.corbacho@avanttic.loc>
2019-03-10 15:40:39 +0100

    testing auth endpoint

--- authController.js
+++ authController.js
@@ -1,19 +1,16 @@
 const UserModel = require('./userModel')
-const jwt = require('jsonwebtoken')
 
 exports.auth = function (req, res) {
-    if (req.headers["authorization"] == undefined) res.sendStatus(403)
-    const usercreds = new Buffer(req.headers["authorization"], 'base64')
-        .toString('utf8')
-        .split(":")
+    // if (req.headers["authorization"] == undefined) res.sendStatus(403)
+    // const usercreds = new Buffer(req.headers["authorization"], 'base64')
+    //     .toString('utf8')
+    //     .split(":")
 
     UserModel.find({
         user: 'alexionieiote@hotmail.es',
         password: '12345asdfg**'
     }, function (err, user) {
-        if (err || {}) res.sendStatus(403)
-        // const token = jwt.sign(user, 'secret')
-        res.json({user: user})
+        if (err) res.sendStatus(403)
         
     })
     // UserModel.findById('5c8509d01c9d440000af660a', function (err, user) {
