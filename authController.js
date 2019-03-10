fb39b57235dbde7bcb09adfd6f3776e1add440a4
Alexis Corbacho <alexis.corbacho@avanttic.loc>
2019-03-10 15:46:50 +0100

    testing auth endpoint

--- authController.js
+++ authController.js
@@ -1,17 +1,17 @@
 const UserModel = require('./userModel')
 
 exports.auth = function (req, res) {
-    // if (req.headers["authorization"] == undefined) res.sendStatus(403)
-    // const usercreds = new Buffer(req.headers["authorization"], 'base64')
-    //     .toString('utf8')
-    //     .split(":")
+    if (req.headers["authorization"] == undefined) res.sendStatus(403)
+    const usercreds = new Buffer(req.headers["authorization"], 'base64')
+        .toString('utf8')
+        .split(":")
 
     UserModel.find({
-        user: 'alexionieiote@hotmail.es',
-        password: '12345asdfg**'
+        user: usercreds[0],
+        password: usercreds[1]
     }, function (err, user) {
-        if (err) res.sendStatus(403)
-        res.json({user: user})
+        if (err || user.length == 0) res.sendStatus(403)
+        res.json({user})
     })
     
 }
